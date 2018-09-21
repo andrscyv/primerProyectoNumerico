2e3c61dda9efa2e69053700eb9445aba3037b362
LUIS FELIPE LANDA LIZARRALDE <llandali@itam.mx>
2018-09-20 20:34:01 -0500

    método QR

---
+++ MQR_simple.m
@@ -0,0 +1,21 @@
+function [lambdas,Q] = MQR_simple(A, k, tol)
+
+[m,n] =size(A);
+
+Q = eye(m,m);
+Qbar=Q;
+R = A;
+cont = 0;
+Am = A
+while(cont<k && norm(diag(Am,-1))>=tol)
+    Am =R*Q;
+    [Q,R] = qr(Am);
+    Qbar = Qbar*Q;
+    cont = cont +1;
+end
+cont
+lambdas = diag(R*Q);
+Q = Qbar;
+
+end
+
