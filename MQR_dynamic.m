5f49d511638a5283ab642f949f890b1564184e8d
MoniiAlba <soeur_azmaria@hotmail.com>
2018-09-23 01:46:47 -0500

    listo shift raro

--- MQR_dynamic.m
+++ MQR_dynamic.m
@@ -6,14 +6,14 @@ lambdas = zeros(m,1);
 index = m;
 contCompTol = 500;   
 contIterac = 0;
-while index > 1
+if(!comparaMatrices(A,A')) %matrices no simetricas
+  while index > 1
     contComp = 0;
     while max(abs(A(index,1:index-1))) > tol && contComp < contCompTol
         contComp = contComp + 1;
         del = 1/2*( A(index-1,index-1)-A(index,index));
         signDel = del/abs(del);
-        %shift = A(index, index)-signDel*(A(index-1,index)^2/(abs(del)+sqrt(del^2+A(index,index-1)^2)));
-        shift = A(index,index);
+        shift = A(index, index)-signDel*(A(index-1,index)^2/(abs(del)+sqrt(del^2+A(index,index-1)^2)));
         identidad = eye(index);
         [Q, R] = qr(A-shift*identidad);
         A =R*Q+shift*identidad;
@@ -30,6 +30,32 @@ while index > 1
         index = index - 2;
         A = A(1:index,1:index);
     end
+  end
+else  %matrices simetricas
+  while index > 1
+      contComp = 0;
+      while max(abs(A(index,1:index-1))) > tol && contComp < contCompTol
+          contComp = contComp + 1;
+          del = 1/2*( A(index-1,index-1)-A(index,index));
+          signDel = del/abs(del);
+          shift = A(index,index);
+          identidad = eye(index);
+          [Q, R] = qr(A-shift*identidad);
+          A =R*Q+shift*identidad;
+          contIterac = contIterac + 1;
+      end
+      if contComp < contCompTol
+          lambdas(index,1) = A(index,index);
+          index = index - 1 ;
+          A = A(1:index, 1:index);
+      else
+          delta = (A(index-1,index-1)-A(index,index))^2 + 4*A(index, index-1)*A(index-1,index);
+          lambdas(index,1) = (A(index-1,index-1)+A(index,index)+sqrt(delta))/2;
+          lambdas(index-1,1) = (A(index-1,index-1)+A(index,index)-sqrt(delta))/2;
+          index = index - 2;
+          A = A(1:index,1:index);
+      end
+  end
 end
 if index > 0 
     lambdas(1,1) = A(1,1);
